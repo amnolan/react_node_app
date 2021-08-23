22fccf2d81180a6841c499c31705a9a5b73a5012
A.M. Nolan <11098245+amnolan@users.noreply.github.com>
2021-08-23 11:11:49 -0700

    Added deployed directory resolution.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 // server/index.js
 
-const express = require("express")
+const path = require('path');
+
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// dynamically serve based on current location
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// handle the GET requests to /api route
 app.get("/api", (req, res) => {
 	res.json({ message: "Hello from server!" });
+});
+
+// all other GET requests
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname), '../client/build','index.html')
 });
 
 app.listen(PORT, ()=>{
